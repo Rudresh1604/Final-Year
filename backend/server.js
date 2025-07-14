014246da107d62cd59c33266647c80c94fb026c2
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-07-14 14:41:10 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dbConnect = require("./config/dbConnect");
+const doctorRoutes = require("./routes/doctorRoutes");
 const app = express();
 const PORT = process.env.PORT || 3003;
-require("./config/dbConnect");
-
 app.use(express.json());
 
 app.use(cors());
+dbConnect()
 
+app.use("/api/doctors", doctorRoutes);
 
 app.listen(PORT, () => {
   console.log(`server started on ${PORT}`);
