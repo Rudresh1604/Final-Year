3d25976a6add9f98de837c7486a08e92c54d97ed
Rudresh1604 <rudreshdharkar@gmail.com>
2025-07-14 14:53:10 +0530

    added patient

--- backend/controllers/patientController.js
+++ backend/controllers/patientController.js
@@ -3,6 +3,8 @@ const Patient = require("../model/patientSchema");
 const createPatient = async (req, res) => {
   const { name, email, phone, gender, password, bloodGroup, age, address } =
     req.body;
+  console.log(req.body);
+
   try {
     const patient = await Patient.create({
       name: name,
