72760b1832cd15de20d90a0ea5ab1c44a19bcc45
Rudresh1604 <rudreshdharkar@gmail.com>
2025-07-14 14:27:59 +0530

    update schema

--- backend/model/appointmentSchema.js
+++ backend/model/appointmentSchema.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 
 const appointmentSchema = new mongoose.Schema({
   patientId: { type: mongoose.Schema.Types, ref: "Patient" },
-  doctorId: { type: mongoose.Schema.Types, ref: "Doctor" }, 
+  doctorId: { type: mongoose.Schema.Types, ref: "Doctor" },
   date: Date,
-  time: String, 
+  time: String,
   status: { type: String, enum: ["Scheduled", "Completed", "Cancelled"] },
   reason: String,
   medicine: [{ name: String, dosage: String, duration: String }],
-  prescription: String, 
+  prescription: String,
   createdAt: Date,
   updatedAt: Date,
 });
 
-const appointment = mongoose.model("Appointment", appointmentSchema);
-module.exports = appointment;
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+module.exports = Appointment;
