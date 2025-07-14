c5ad43bdea1364278ba65c28d4a0923d62518739
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-07-14 14:09:51 +0530

    Added files

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const RequestUrl = process.env.REQUEST_URL || process.env.REQUEST_URL1 || 3000;
 const PORT = process.env.PORT || 3003;
 require("./config/dbConnect");
 
-const portfolioRoute = require("./Routes/portFolio");
 app.use(express.json());
 
 app.use(cors());
 
-app.use("/api/portfolio", portfolioRoute);
 
 app.listen(PORT, () => {
   console.log(`server started on ${PORT}`);
