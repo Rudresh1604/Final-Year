88efef7daa7299feae17b34f436d84ab7a4b2789
Atharva-1703 <141930344+Atharva-1703@users.noreply.github.com>
2025-07-29 11:24:01 +0530

    added delete doctor

--- backend/routes/doctorRoutes.js
+++ backend/routes/doctorRoutes.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const { addDoctor, addDoctorSlot, getDoctorById, getDoctors, updateDoctor } = require("../controllers/doctorController");
+const { addDoctor, addDoctorSlot, getDoctorById, getDoctors, updateDoctor ,deleteDoctor} = require("../controllers/doctorController");
 const doctorRoutes = express.Router();
 
+// add Doctor
 doctorRoutes.post("/add", addDoctor);
+
+// add slot
 doctorRoutes.post("/slots", addDoctorSlot);
+
+// get Doctor by id
 doctorRoutes.get("/:id",getDoctorById);
+
+// get Doctors
 doctorRoutes.get("/",getDoctors)
+
+// update Doctor
 doctorRoutes.put("/update/:id",updateDoctor)
 
+// delete Doctor
+doctorRoutes.delete("/delete/:id",deleteDoctor)
 
 module.exports = doctorRoutes;
