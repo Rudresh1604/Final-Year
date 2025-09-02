4a5acc775a3267ffb49baa0f72bfa68f71ea0847
Rudresh1604 <rudreshdharkar@gmail.com>
2025-09-02 12:01:08 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 app.use(express.json());
 
-app.use(cors());
+app.use(cors("*"));
 dbConnect();
 
 app.use("/api/appointment", appointmentRoutes);
