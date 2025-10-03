6304c6c73499229b85068db9b0a7704de524bdc9
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-10-03 19:23:46 +0530

    added about section

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/Home/HeroSection'
 import ServicesSection from '../components/Home/ServiceSection'
+import AboutSection from '../components/Home/AboutSection'
 
 const Home = () => {
   return (
     <div className="font-sans bg-gray-50 text-gray-800">
       <HeroSection />
       <ServicesSection />
-      {/* <AboutUsSection /> */}
+      <AboutSection />
       {/* <LatestNewsSection /> */}
       {/* <FAQSection /> */}
     </div>
