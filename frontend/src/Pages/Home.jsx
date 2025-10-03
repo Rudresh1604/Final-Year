f1a76d269deb422c2cff067be5d361a1e26ac306
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-10-03 18:52:49 +0530

    added hero section for home page

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from '../components/Home/HeroSection'
+
+const Home = () => {
+  return (
+    <div className="font-sans bg-gray-50 text-gray-800">
+      <HeroSection />
+      {/* <ServicesSection />
+      <AboutUsSection />
+      <LatestNewsSection />
+      <FAQSection /> */}
+    </div>
+  )
+}
+
+export default Home
