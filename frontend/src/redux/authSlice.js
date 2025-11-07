6b5b0ac421c9b5af24dfe4417f511f6edba82c83
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-11-07 22:11:01 +0530

    added auth slice

---
+++ frontend/src/redux/authSlice.js
@@ -0,0 +1,117 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+const API_URL = "https://final-year-1-1ra8.onrender.com";
+
+// LOGIN
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async ({ email, password, role }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${API_URL}/login`, {
+        email,
+        password,
+        role,
+      });
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      return { token, role };
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data?.message || "Login failed. Please try again."
+      );
+    }
+  }
+);
+
+// REGISTER DOCTOR
+export const registerDoctor = createAsyncThunk(
+  "auth/registerDoctor",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(`${API_URL}/doctors`, formData);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.message || "Doctor registration failed."
+      );
+    }
+  }
+);
+
+// REGISTER PATIENT
+export const registerPatient = createAsyncThunk(
+  "auth/registerPatient",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const res = await axios.post(`${API_URL}/patient`, formData);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(
+        err.response?.data?.message || "Patient registration failed."
+      );
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    token: localStorage.getItem("token") || null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem("token");
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // LOGIN
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.token = action.payload.token;
+        state.error = null;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      //   REGISTER DOCTOR
+      .addCase(registerDoctor.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerDoctor.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(registerDoctor.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      // REGISTER PATIENT
+      .addCase(registerPatient.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerPatient.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(registerPatient.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logout } = authSlice.actions;
+export default authSlice.reducer;
