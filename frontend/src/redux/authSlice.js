c44226b23f9123aa9514c05f0335e33fda5fb1dd
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2025-11-08 10:43:58 +0530

    added login redirect for already having account

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-const API_URL = "https://final-year-1-1ra8.onrender.com";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 // LOGIN
 export const loginUser = createAsyncThunk(
