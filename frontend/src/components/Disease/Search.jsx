41e75c17343b3419895fadbe650eb6f668b13e88
Sachin Choudhary <sachinchoudhary97599@gmail.com>
2026-02-25 20:00:46 +0530

    added search disease

--- frontend/src/components/Disease/Search.jsx
+++ frontend/src/components/Disease/Search.jsx
@@ -1,8 +1,26 @@
+import axios from "axios";
 import { Search } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
-const SearchDisease = () => {
-  const [searchQuery, setSearchQuery] = useState(null);
+const SearchDisease = ({ setDiseaseList }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  useEffect(() => {
+    const fetchDiseases = async () => {
+      try {
+        const res = await axios.get(
+          `${API_URL}/api/disease?search=${searchQuery}`,
+        );
+
+        if (res.data.success) {
+          setDiseaseList(res.data.diseases);
+        }
+      } catch (error) {
+        console.error("Search error:", error);
+      }
+    };
+    fetchDiseases();
+  }, [searchQuery]);
   return (
     <div className="flex max-sm:flex-col flex-row items-center justify-between">
       <label className="mb-2 block text-xl text-gray-700">
@@ -15,7 +33,7 @@ const SearchDisease = () => {
           onChange={(e) => setSearchQuery(e.target.value)}
           placeholder="Search Disease"
           value={searchQuery}
-          className="w-full p-1   focus:outline-none"
+          className="w-full p-1 focus:outline-none"
         />
       </div>
     </div>
